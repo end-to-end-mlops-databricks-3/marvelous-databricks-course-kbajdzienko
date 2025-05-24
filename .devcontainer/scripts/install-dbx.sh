9e9c365c21d2be965790e6d26936e4b5d5bfb72c
Krzysztof <krzysztof.bajdzienko@gmail.com>
2025-05-24 17:30:12 +0000

    devcontainer - install spark

--- .devcontainer/scripts/install-dbx.sh
+++ .devcontainer/scripts/install-dbx.sh
@@ -1,2 +1,9 @@
 curl -fsSL https://raw.githubusercontent.com/databricks/setup-cli/main/install.sh | sh
 databricks completion bash > /etc/bash_completion.d/databricks
+
+#Install Apache Spark with Hadoop
+curl -fsSL https://archive.apache.org/dist/spark/spark-3.4.1/spark-3.4.1-bin-hadoop3.tgz | tar xz -C /opt/
+ln -s /opt/spark-3.4.1-bin-hadoop3 /opt/spark
+# Set environment variables for Spark
+echo 'export SPARK_HOME=/opt/spark' >> /etc/bash.bashrc
+echo 'export PATH=$SPARK_HOME/bin:$PATH' >> /etc/bash.bashrc
