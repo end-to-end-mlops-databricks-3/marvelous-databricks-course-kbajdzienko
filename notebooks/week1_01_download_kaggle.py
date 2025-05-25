90de7522680db159334dc27568bf956e6012ebb2
Krzysztof <krzysztof.bajdzienko@gmail.com>
2025-05-25 06:45:23 +0000

    Refactor Kaggle data download process: use uv for imports in dbx, and utilize KaggleDownloader for file retrieval

--- notebooks/week1_01_download_kaggle.py
+++ notebooks/week1_01_download_kaggle.py
@@ -5,33 +5,45 @@
 # MAGIC This notebooks is an excercise to practice loading data from external API's to Delta. The motivation for using delta tables is to exploring the lineage and integration between data engineering and ML workflows.
 # MAGIC This notebook will require `kaggle` package to be installed in the cluster.
 
+# COMMAND ----------
+#%pip install uv
+
+# COMMAND ----------
+import os
+os.environ["UV_PROJECT_ENVIRONMENT"] = os.environ["VIRTUAL_ENV"]
+
+# COMMAND ----------
+%sh uv sync --locked --no-editable
+
+# COMMAND ----------
+#%restart_python
+
+# COMMAND ----------
+from pathlib import Path
+import sys
+sys.path.append(str(Path.cwd().parent / 'src'))
 
 
 # COMMAND ----------
-import kaggle
-from os import mkdir
+from pyspark.dbutils import DBUtils
+from pyspark.sql import SparkSession
+from house_price.kaggle_downloader import KaggleDownloader
+from house_price.utils import utils
 
 # COMMAND ----------
-dataset = dbutils.widgets.get("dataset")
-dataset_files = ["train.csv", "test.csv"]
-catalog = dbutils.widgets.get("catalog")
-schema = dbutils.widgets.get("schema")
-download_path = f"/Volumes/{catalog}/{schema}/raw_kaggle/{dataset}"
+spark = SparkSession.builder.getOrCreate()
+dbutils = DBUtils(spark)
 
 # COMMAND ----------
-# Create the download path if it does not exist
-try:
-    mkdir(download_path)
-except FileExistsError:
-    print(f"Directory {download_path} already exists.")
-except OSError:
-    print(f"Creation of the directory {download_path} failed.")
-else:
-    print(f"Successfully created the directory {download_path}.")
+# Test if volume exists
+downloader = KaggleDownloader(
+    kaggle_dataset=dbutils.widgets.get("kaggle_dataset"),
+    dataset_files=dbutils.widgets.get("dataset_file_names").split(","),
+    catalog=dbutils.widgets.get("catalog"),
+    schema=dbutils.widgets.get("schema"),
+    volume=dbutils.widgets.get("volume"),
+    databricks=utils.is_databricks()
+)
 
-# COMMAND ----------
-for file in dataset_files:
-    k = kaggle.KaggleApi()
-    k.authenticate()
-    k.competition_download_file(competition=dataset, file_name=file, path=download_path)
+downloader.download()
 
