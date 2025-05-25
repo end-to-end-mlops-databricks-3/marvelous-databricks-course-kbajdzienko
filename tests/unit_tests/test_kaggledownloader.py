9ddc77784864991b7437ce13e86d9e4448ab01b5
Krzysztof <krzysztof.bajdzienko@gmail.com>
2025-05-25 17:40:47 +0000

    Remove download check from KaggleDownloader unit test

--- tests/unit_tests/test_kaggledownloader.py
+++ tests/unit_tests/test_kaggledownloader.py
@@ -1,7 +1,5 @@
 """Unit tests for KaggleDownloader class."""
 
-import os
-
 import pandas as pd
 import pytest
 
@@ -11,8 +9,6 @@ from house_price.kaggle_downloader import KaggleDownloader
 def test_local_path_and_env(downloader_local: KaggleDownloader) -> None:
     """Test local path creation and env loading."""
     assert downloader_local.download_path.endswith("data/mlops-test/krzyszto/raw_kaggle")
-    downloader_local.download()
-    assert os.path.exists(downloader_local.download_path)
 
 
 def test_init_missing_env(monkeypatch: pytest.MonkeyPatch) -> None:
